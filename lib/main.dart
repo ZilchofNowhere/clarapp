c05a72ed2387c0e3e970168726979659dca59c43
Atakankk <atakankrcn@gmail.com>
2022-06-23 01:21:45 +0300

    your commit message

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: 'Clara',
       theme: ThemeData(
         primarySwatch: Colors.red,
