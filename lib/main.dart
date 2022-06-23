d3bd4b03c7c46d465c6ffec3b6546a3041ed6300
Atakankk <atakankrcn@gmail.com>
2022-06-23 14:58:10 +0300

    logscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:clarapp/screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import "package:firebase_core/firebase_core.dart";
+import "./screens/login_screen.dart";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
